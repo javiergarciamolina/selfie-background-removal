67c07a27a70526e69dd9ecc5c96d4c590a36c373
Javier Garcia Molina <javiergarciamolina@gmail.com>
2021-02-11 13:07:32 +0100

    Update background-removal-app.py

--- background-removal-app.py
+++ background-removal-app.py
@@ -175,7 +175,7 @@ def main():
 
       image = orig_image.resize((224,224))
       image = np.array(img_to_array(image))
-      image = correct_orientation(image)
+#       image = correct_orientation(image)
    
       image = np.array(image) / 255
       image = np.expand_dims(image, axis=0)
